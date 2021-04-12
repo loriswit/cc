e45ac6f68ad3d9b65f3b485219c77f73c6dcfd2c
Loris Witschard <loris.wit@gmail.com>
2021-04-12 18:32:46 +0200

    project: add run script

---
+++ project/run.sh
@@ -0,0 +1,11 @@
+export DB_HOST=localhost
+export DB_PORT=3306
+export DB_DBNAME=watches
+export DB_USER=watches
+export DB_PASS=watches
+
+export HTTP_USER=cloud
+export HTTP_PASS=computing
+
+export FLASK_APP=server.py
+flask run --port 1080
