754dda5bb88f2783903d7bb54cec80cf5257798d
Loris Witschard <loris.wit@gmail.com>
2021-05-29 22:50:04 +0200

    project: pull latest images when deploying

--- project/deploy.sh
+++ project/deploy.sh
@@ -6,3 +6,7 @@ docker push loriswit/watches-image-v1
 
 # create/update resources in cluster
 kubectl apply -f all.yml
+
+# restart deployments to pull latest images
+kubectl rollout restart deployment/info-v1
+kubectl rollout restart deployment/image-v1
