894984595214c8291cb7bf257e8d6ad6ee2f3729
Loris Witschard <loris.wit@gmail.com>
2021-05-31 17:48:38 +0200

    project: wait for table to be created before importing

--- project/info-v2/initialize_dynamodb.sh
+++ project/info-v2/initialize_dynamodb.sh
@@ -6,17 +6,19 @@ export AWS_PAGER=""
 TABLE_NAME="${TABLE_NAME:-watches}"
 
 # check if table already exists
-TABLE_EXISTS=$(aws dynamodb list-tables | grep -c "$TABLE_NAME" | cat )
-if (( TABLE_EXISTS )); then
+if aws dynamodb list-tables | grep -q "$TABLE_NAME"; then
     echo "Table '$TABLE_NAME' already exists. Skipping table creation."
 else
     echo "Creating new table '$TABLE_NAME'..."
     aws dynamodb create-table \
-        --table-name watches \
+        --table-name "$TABLE_NAME" \
         --attribute-definitions AttributeName=sku,AttributeType=S \
         --key-schema AttributeName=sku,KeyType=HASH \
         --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
         > /dev/null
+
+    # wait for table to be created
+    aws dynamodb wait table-exists --table-name "$TABLE_NAME" > /dev/null
 fi
 
 # fill table with items from watches.json
