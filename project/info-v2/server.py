5dae218c849408213a8735c5d705c26563aacf6e
Loris Witschard <loris.wit@gmail.com>
2021-05-31 16:31:52 +0200

    project: add CORS support

--- project/info-v2/server.py
+++ project/info-v2/server.py
@@ -9,25 +9,35 @@ dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table("watches")
 
 
+# generate response supporting CORS
+def make_response(event: dict, code: int, body={}):
+    response = {"statusCode": code}
+    if "origin" in event["headers"]:
+        response["headers"] = {"Access-Control-Allow-Origin": event["headers"]["origin"]}
+    if body:
+        response["body"] = json.dumps(body)
+    return response
+
+
 # POST /watch: add a new watch to the store
 def create(event, context):
     # parse JSON from body
     try:
         body = json.loads(event["body"])
     except Exception as e:
-        return {
-            "statusCode": http.HTTPStatus.BAD_REQUEST,
-            "body": json.dumps({"error": "malformed request: " + str(e)})
-        }
+        return make_response(
+            event, http.HTTPStatus.BAD_REQUEST,
+            {"error": "malformed request: " + str(e)}
+        )
 
     # check required fields
     required = ["sku", "type", "status", "gender", "year"]
     missing = set(required) - set(body.keys())
     if missing:
-        return {
-            "statusCode": http.HTTPStatus.BAD_REQUEST,
-            "body": json.dumps({"error": "missing required fields", "missing": list(missing)})
-        }
+        return make_response(
+            event, http.HTTPStatus.BAD_REQUEST,
+            {"error": "missing required fields", "missing": list(missing)}
+        )
 
     # only keep the following fields
     allowed_fields = ["bracelet_material", "case_form", "case_material", "dial_color",
@@ -39,10 +49,10 @@ def create(event, context):
     for k, v in filtered.items():
         t = numbers.Number if k == "year" else str
         if not isinstance(v, t):
-            return {
-                "statusCode": http.HTTPStatus.BAD_REQUEST,
-                "body": json.dumps({"error": f"field '{k}' must be of type {t.__name__}"})
-            }
+            return make_response(
+                event, http.HTTPStatus.BAD_REQUEST,
+                {"error": f"field '{k}' must be of type {t.__name__}"}
+            )
 
     # check enum values
     allowed_values = {
@@ -52,28 +62,23 @@ def create(event, context):
     }
     for k, v in allowed_values.items():
         if k in filtered.keys() and filtered[k] not in allowed_values[k]:
-            return {
-                "statusCode": http.HTTPStatus.BAD_REQUEST,
-                "body": json.dumps({
+            return make_response(
+                event, http.HTTPStatus.BAD_REQUEST, {
                     "error": f"field '{k}' must be one of the enumerated values",
                     "enum": allowed_values[k]
                 })
-            }
 
     # insert item into database
     table.put_item(Item=filtered)
-    return {"statusCode": http.HTTPStatus.OK}
+    return make_response(event, http.HTTPStatus.OK)
 
 
 # GET /watch/{sku}: return watch data
 def read(event, context):
     response = table.get_item(Key={"sku": event["pathParameters"]["sku"]})
     if "Item" not in response:
-        return {"statusCode": http.HTTPStatus.NOT_FOUND}
+        return make_response(event, http.HTTPStatus.NOT_FOUND)
 
     # convert decimals to ints to prevent JSON serialization errors
     watch = {k: int(v) if isinstance(v, decimal.Decimal) else v for k, v in response["Item"].items()}
-    return {
-        "body": json.dumps(watch),
-        "statusCode": http.HTTPStatus.OK
-    }
+    return make_response(event, http.HTTPStatus.OK, watch)
